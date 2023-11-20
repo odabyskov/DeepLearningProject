8ec3670aad7a1d5741cde5d6ecea97fe10d7a417
odabyskov <s183700@student.dtu.dk>
2023-11-20 10:07:03 +0100

    cleaning

--- test/load_model.py
+++
@@ -1,8 +0,0 @@
-import torch
-import numpy as np
-import os
-from ase import Atoms
-
-qm9tut = 'test/qm9tut'
-best_model = torch.load(os.path.join(qm9tut, 'best_inference_model'), map_location=torch.device('cpu'))
-print(best_model)
